02f4eab311ac6e7d520e33ee39953d08b6f5557d
Yash Sinha <55059067+Hanuusinha@users.noreply.github.com>
2022-07-09 11:13:52 +0530

    Create StockBuyAndSell.cpp

---
+++ StockBuyAndSell.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h> 
+int maximumProfit(vector<int> &prices){
+    int maxPro = 0, minPrice = INT_MAX;
+    for(int i = 0 ; i < prices.size() ; i++)
+    {
+        minPrice = min(minPrice,prices[i]);
+        maxPro = max(maxPro , prices[i] - minPrice);
+    }
+    return maxPro;
+}
