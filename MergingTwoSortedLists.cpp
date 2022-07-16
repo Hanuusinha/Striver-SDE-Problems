76d4668895c5c3b0268658aba21da7ece11cf455
Yash Sinha <55059067+Hanuusinha@users.noreply.github.com>
2022-07-17 00:15:07 +0530

    Update MergingTwoSortedLists.cpp

--- MergingTwoSortedLists.cpp
+++ MergingTwoSortedLists.cpp
@@ -82,3 +82,47 @@ Node<int>* sortTwoLists(Node<int>* first, Node<int>* second)
     traversal->next = NULL;
     return result;
 }
+//Using recursive approach:
+#include <bits/stdc++.h> 
+/************************************************************
+
+    Following is the linked list node structure.
+    
+    template <typename T>
+    class Node {
+        public:
+        T data;
+        Node* next;
+
+        Node(T data) {
+            next = NULL;
+            this->data = data;
+        }
+
+        ~Node() {
+            if (next != NULL) {
+                delete next;
+            }
+        }
+    };
+
+************************************************************/
+
+Node<int>* sortTwoLists(Node<int>* first, Node<int>* second)
+{
+    if(first == NULL)return second;
+    if(second == NULL)return first;
+    Node<int>* result = NULL;
+    if(first->data<=second->data)
+    {
+        result = first;
+        result->next = sortTwoLists(first->next,second);
+        
+    }else{
+        result = second;
+        result->next = sortTwoLists(first,second->next);
+    }
+    return result;
+    // Write your code here.
+}
+
