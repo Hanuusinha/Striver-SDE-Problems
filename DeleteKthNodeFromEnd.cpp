49fca1ebf01ce6d60ec5167639d099cc287a55ea
Yash Sinha <55059067+Hanuusinha@users.noreply.github.com>
2022-07-18 16:35:28 +0530

    Update DeleteKthNodeFromEnd.cpp

--- DeleteKthNodeFromEnd.cpp
+++ DeleteKthNodeFromEnd.cpp
@@ -56,3 +56,46 @@ LinkedListNode<int>* removeKthNode(LinkedListNode<int> *head, int K)
     
     // Write your code here.
 }
+//WITHOUT FINDING THE SIZE OF THE LINKEDLIST:
+#include <bits/stdc++.h> 
+/****************************************************************
+
+    Following is the class structure of the LinkedListNode class:
+
+    template <typename T>
+    class LinkedListNode
+    {
+    public:
+        T data;
+        LinkedListNode<T> *next;
+        LinkedListNode(T data)
+        {
+            this->data = data;
+            this->next = NULL;
+        }
+    };
+
+*****************************************************************/
+
+LinkedListNode<int>* removeKthNode(LinkedListNode<int> *head, int K)
+{
+    LinkedListNode<int> *start = new LinkedListNode<int>(1);
+    LinkedListNode<int>* fast = start,*slow = start;
+    start->next = head;
+    if(head == NULL or K == 0)
+    {
+        return head;
+    }
+    for(int i = 1 ; i <= K ; ++i)
+    {
+        fast = fast->next;
+    }
+    while(fast->next != NULL)
+    {
+        fast = fast->next;
+        slow = slow->next;
+    }
+    slow->next = slow->next->next;
+    // Write your code here.
+    return start->next;
+}
